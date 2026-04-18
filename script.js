dc6766b8481a5c5cb8e04962022263d6460eb55c
SoyuzDangol <dangolsoyuz@gmail.com>
2026-04-18 21:48:46 +0545

    Add complete UI for weather app

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputBox = document.getElementById('SearchBar');
+const searchbtn = document.getElementById('button1');
+const weather_img = document.getElementById('weather-image');
+const temperature = document.getElementById('temperature');
+const description =document.getElementById('Description');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById(wind-speed);
